b47d2dc9f950f8b4b5b56228b43975fcff0d83c6
maike-hilda <maikescherer@gmail.com>
2017-07-30 17:00:55 -0700

    all project requirements done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,6 +16,9 @@ var database = firebase.database();
 //store data
 $("#add-train-btn").on("click", addToDatabase);
 
+database.ref().on("child_added", databaseToHTML);
+
+
 function addToDatabase() {
 	event.preventDefault();
 
@@ -44,30 +47,35 @@ function addToDatabase() {
 	$("#frequency").val("");
 };
 
-
-//database to html
-database.ref().on("child_added", function(snapshot, prevChildKey) {
-
-
+function databaseToHTML(snapshot, prevChildKey) {
 	//console.log(snapshot.val());
 
 	var trainName = snapshot.val().trainName;
 	var destination = snapshot.val().destination;
 	var firstTrain = snapshot.val().firstTrain;
 	var freq = snapshot.val().freq; 
+	
+	var remainder; 
+	var nextArrival;
 
-	//var remAndNext = trainLogic(firstTrain, freq);	
-	var nextArrival = "00:00"; //remAndNext[1];
-	var remainder = 3; //remAndNext[0];
+
+	[remainder, nextArrival] = trainLogic(firstTrain, freq);	
 
 	//format time
-	// if (nextArrival[0] <= 12) {
-	// 	nextArrival = nextArrival[0] + ":" + nextArrival[1] + " AM";
-	// }
-	// else {
-	// 	nextArrival = nextArrival[1]-12 + ":" + nextArrival[1] + " PM";
-	// };
-	moment(nextArrival).format("hh:mm:ss A");
+	if (nextArrival[1] < 10) {
+		nextArrival[1] = "0" + nextArrival[1];
+	};
+
+	if (nextArrival[0] <= 12) {
+		nextArrival = nextArrival[0] + ":" + nextArrival[1] + " AM";
+	}
+	else {
+		nextArrival = nextArrival[0] + ":" + nextArrival[1] + " PM";
+	}
+	// nextArrival = nextArrival[0] + ":" + nextArrival[1];
+	// console.log(nextArrival);
+	// nextArrival = moment(nextArrival).format("hh:mm A");
+	// console.log(nextArrival);
 
 	//add to table
 	$("#train-schedule-table > tbody").append("<tr><td>" + trainName + "</td>" +
@@ -77,43 +85,51 @@ database.ref().on("child_added", function(snapshot, prevChildKey) {
 												"<td>" + remainder + "</td></tr>");
 
 
-});
+};
 
 
 //next train time
-function trainLogic(first, freq) {
-	var currentTime = moment().format("hh:mm");
-	currentTime.splice(":");
-	first.splice(":");
+function trainLogic(firstTrain, freq) {
+	var currentTime = moment().format("HH:mm");
+	currentTime = currentTime.split(":");
+	currentTime = [ parseInt(currentTime[0]), parseInt(currentTime[1]) ];
+	firstTrain = firstTrain.split(":");
+	firstTrain = [ parseInt(firstTrain[0]), parseInt(firstTrain[1]) ];
 
-	var currentTimeInMin = currentTime[0]*60 + currentTime[1];
-	var firstInMin = first[0]*60 + first[1];
+	//convert time to minutes
+	var currentTimeMin = currentTime[0]*60 + currentTime[1];
+	var firstTrainMin = firstTrain[0]*60 + firstTrain[1];
+
 	var remainder = 0;
 	var nextArrival = currentTime;
 
 	//find remainder
-	if (firstInMin > currentTimeInMin) {
-		remainder = (firstInMin - currentTimeInMin) % freq;
-		console.log(currentTime[0]);
-		// var currFreq = frequency - remainder;
-		// currFreq += currentTime[1];
+	if (firstTrainMin > currentTimeMin) {
+		remainder = (firstTrainMin + 24*60 - currentTimeMin) % freq;
+	}
+	else if (firstTrainMin < currentTimeMin){
+		remainder = (currentTimeMin - firstTrainMin) % freq;
+	}
+	// no change if firstTrainMin === currentTrainMin
 
-		nextArrival = [currentTime[0], currentTime[1] + (freq - remainder) ];
-	}
-	else if (firstInMin === currentTimeInMin) {
-		//nothing
-	}
-	else if (firstInMin < currentTimeInMin){
-		remainder = (currentTimeInMin + 24*60 - firstInMin) % freq;
-		nextArrival = [currentTime[0], currentTime[1]+(freq - remainder)];
+	var nextArrivalMin = currentTime[0] + remainder;
+	var freqMinusRem = freq - remainder;
+
+	if (remainder === 0) {
+		remainder = "Arriving";
+		return [remainder, nextArrival];
 	};
 
+	if (nextArrivalMin < 60) {
+		nextArrival = [ currentTime[0],	currentTime[1]+freqMinusRem ];	
+
+	}
+	else if (nextArrivalMin > 60) {
+		nextArrival = [ currentTime[0]+1, currentTime[1]+freqMinusRem-60 ];
+	};	
+	
 	return [remainder, nextArrival];
 }; 
-
-
-
-
 
 }); 
 
